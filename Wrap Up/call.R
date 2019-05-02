fdc89e3a54a9d3721f761e5beae9534197d8859d
ShivtejPatil <37193739+skynetshivtej@users.noreply.github.com>
2019-05-02 10:02:35 +0100

    Call History

--- Wrap Up/call.R
+++ Wrap Up/call.R
@@ -3,15 +3,25 @@ require(lubridate)
 
 data <- read.csv(file = "C:\\Users\\Shivtej Patil\\Documents\\Call History\\Call History.csv", header = FALSE, sep = ",")
 
-require(dplyr)
 
+require(stringr)
 #Select the rows which are only relelated to Mytaxi
 data <- data %>% filter(str_detect(data$V1, "MyTaxi"))
 
+require(dplyr)
 # Remove Wrapup Code which are Non-Dialer Coll, Agent Skip, Agent Logout, Ambiguous
 data <- data[(!data$V16 == "Non-Dialer Call" & !data$V16 == "Agent Skip" & !data$V16 == "Agent Logout" & !data$V16 == "Ambiguous"),]
 
+dateeu <- data[, 4]
 
+require(data.table)
 #Convert DateTime format into the Date format 
 dateeu <- as.Date(dateeu, format = "%d/%m/%Y %H:%M:%S")
+
+callanstime<- data[,5]
+callanstime <-as.Datetime(callanstime,format="%d/%m/%Y %H:%M:%S")
+
+#Extract Timing From DataTime Datatype UTC Time Zome
+callanstime <- as.ITime(as.POSIXct(callanstime, format = "%d/%m/%Y %H:%M:%S", tz = "UTC"))
+
 
