6a53f73828f4e536a3e152494f4e2c99e28672f3
ShivtejPatil <37193739+skynetshivtej@users.noreply.github.com>
2019-05-02 16:22:26 +0100

    Call

--- Wrap Up/call.R
+++ Wrap Up/call.R
@@ -18,35 +18,38 @@ require(data.table)
 #Convert DateTime format into the Date format 
 dateeu <- as.Date(dateeu, format = "%d/%m/%Y %H:%M:%S")
 
-callanstime <- data[,5]
+callanstime <- data[, 5]
 
 
 #Extract Timing From DataTime Datatype UTC Time Zome Call Answered 
-callanstime <- as.ITime(as.POSIXct(callanstime, format = "%d/%m/%Y %H:%M:%S", tz = "UTC"))
+callanstime <- strptime(x = as.character(data$V8), format = "%d/%m/%Y %H:%M:%S",tz= "UTC")
 
 #Extract Timing From DataTime Datatype UTC Time Zome Call Disconnected 
 calldistime <- data[, 8]
-calldistime <- as.ITime(as.POSIXct(calldistime, format = "%d/%m/%Y %H:%M:%S", tz = "UTC"))
+
+calldistime <- strptime(x = as.character(data$V5), format = "%d/%m/%Y %H:%M:%S", tz = "UTC")
 
 # Change logical values into integer for better mathemathical operations 
 data$V11 <- as.integer(as.logical(data$V11))
 data$V12 <- as.integer(as.logical(data$V12))
 
 
-V16 <- c("Driver on Leave","Not Interested","No longer a Taxi Driver","Incorrect Number","Do Not Contact","Agreed to Increase Usage","Agreed to Register / Re - Activate",
-                     "No Answer","DMC Scheduled Callback","Non DMC Scheduled Callback","Agent Skip","Wrong Party","NS","Customer Disconnected","DMC Manual Callback","Registered / Reactivated",
-                     "Happy with mytaxi - No Customers in Area","Non - Dialer Call","Issue with T & Cs","Non DMC Manual Callback","Reactivated","Registered","Customer Query",
-                     "Passenger Issue","Passenger Query","Payment Issue","App Issue","General Information","Currently Using MyTaxi App","Topaz Query","Acquisition - New","Acquisition - 3 T",
-                     "Acquisition - 100 T","Reactivation - New","Reactivation - 3 T","Reactivation - 100 T","Issue with Fare")
+V16 <- c("Driver on Leave", "Not Interested", "No longer a Taxi Driver", "Incorrect Number", "Do Not Contact", "Agreed to Increase Usage", "Agreed to Register / Re - Activate",
+                     "No Answer", "DMC Scheduled Callback", "Non DMC Scheduled Callback", "Agent Skip", "Wrong Party", "NS", "Customer Disconnected", "DMC Manual Callback", "Registered / Reactivated",
+                     "Happy with mytaxi - No Customers in Area", "Non - Dialer Call", "Issue with T & Cs", "Non DMC Manual Callback", "Reactivated", "Registered", "Customer Query",
+                     "Passenger Issue", "Passenger Query", "Payment Issue", "App Issue", "General Information", "Currently Using MyTaxi App", "Topaz Query", "Acquisition - New", "Acquisition - 3 T",
+                     "Acquisition - 100 T", "Reactivation - New", "Reactivation - 3 T", "Reactivation - 100 T", "Issue with Fare")
 
 Contact <- c(1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 DMC <- c(1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
 Sale <- c(0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0)
 
+data <- cbind(data, dateeu, callanstime, calldistime)
 
 dmcsaltable <- data.frame(Localizedwrapup, Contact, DMC, Sale)
 
 require(sqldf)
 
 data <- sqldf("select * from data join dmcsaltable on V16 = Localizedwrapup")
+
 
